51f4cb1a92103130c898512ff2e885b4983eab3b
Artemet <60576459+Artemet@users.noreply.github.com>
2023-01-02 16:33:44 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,118 +1,134 @@
 <?php
-    include "layout/header.php";
-    include "layout/header_line.php";
+include "layout/header.php";
+include "layout/header_line.php";
 ?>
-<div class="web_title">
-    <h2>ready <br> to work<span>.</span></h2>
-</div>
-<div class="about_me_container">
-    <div class="line"></div>
-    <p id="about_me">Hello, I am Artemka. I am currently 18 years old. I actively work
-        in a company and want to earn extra money
-        on freelance exchanges such as up work and others</p>
-</div>
-<div class="work_pluses_container">
-    <h2>pluses of my work</h2>
-    <div class="plus_part">
-        <div class="plus">
-            <p class="number">01</p>
+    <div class="web_title">
+        <h2>ready <br> to work<span>.</span></h2>
+        <div class="double_information">
             <div class="line"></div>
-            <h3 class="plus_name">good price</h3>
-            <p class="plus_description">
-                I can do any work for you for good price.<br>
-                I will not try to get more mony for you.<br>
-                For me the main thing is the quality of work for any money.
-            </p>
+            <p>the site was made with the aim that freelance clients could come
+                in and see what I am good for. Leave a comment below after viewing.</p>
         </div>
-        <div class="img">
-            <img src="res/forest.jpg" alt="">
-        </div>
-    </div>
-    <div class="plus_part">
-        <div class="img">
-            <img src="res/forest_snow.jpg" alt="">
-        </div>
-        <div class="plus">
-            <p class="number">02</p>
-            <div class="line"></div>
-            <h3 class="plus_name">good quality</h3>
-            <p class="plus_description">
-                I believe that I do my job well.<br>
-                I am ready to sit all day at the code editor,<br>
-                but the main thing is to complete the task.
-            </p>
-        </div>
-    </div>
-    <div class="plus_part">
-        <div class="plus">
-            <p class="number">03</p>
-            <div class="line"></div>
-            <h3 class="plus_name">do with love</h3>
-            <p class="plus_description">
-                I put a lot of effort into any job, <br>
-                even if it pays little
-            </p>
-        </div>
-        <div class="img">
-            <img src="res/green_forest.jpg" alt="">
-        </div>
-    </div>
-    <div class="line"></div>
-    <p class="up_work_link">for more information go on <a href="https://www.upwork.com/freelancers/~01517b1af176e5a167" title="my account">up work account</a></p>
-</div>
-<div class="ready_projects">
-    <h2>my ready projects</h2>
-    <div class="projects_cards">
-        <div class="project_card">
-            <div class="img_background">
-                <img src="res/cargox_web.png" alt="">
+        <div class="network_line">
+            <div class="img">
+                <img src="res/instagramm_icon.png" alt="">
             </div>
-            <div class="project_information">
-                <div class="information">
-                    <p class="project_name">cargox web</p>
-                    <div class="line"></div>
-                    <p class="made_time">2022</p>
-                </div>
+            <div class="img">
+                <img src="res/tik_tok_icon.png" alt="">
             </div>
-        </div>
-        <div class="project_card">
-            <div class="img_background">
-                <img src="res/cargox_web.png" alt="">
-            </div>
-            <div class="project_information">
-                <div class="information">
-                    <p class="project_name">cargox web</p>
-                    <div class="line"></div>
-                    <p class="made_time">2022</p>
-                </div>
-            </div>
-        </div>
-        <div class="project_card">
-            <div class="img_background">
-                <img src="res/cargox_web.png" alt="">
-            </div>
-            <div class="project_information">
-                <div class="information">
-                    <p class="project_name">cargox web</p>
-                    <div class="line"></div>
-                    <p class="made_time">2022</p>
-                </div>
-            </div>
-        </div>
-        <div class="project_card">
-            <div class="img_background">
-                <img src="res/cargox_web.png" alt="">
-            </div>
-            <div class="project_information">
-                <div class="information">
-                    <p class="project_name">cargox web</p>
-                    <div class="line"></div>
-                    <p class="made_time">2022</p>
-                </div>
+            <div class="img">
+                <img src="res/vk_icon.png" alt="">
             </div>
         </div>
     </div>
-</div>
+    <div class="about_me_container">
+        <div class="line"></div>
+        <p id="about_me">Hello, I am Artemka. I am currently 18 years old. I actively work
+            in a company and want to earn extra money
+            on freelance exchanges such as up work and others</p>
+    </div>
+    <div class="work_pluses_container">
+        <h2>pluses of my work</h2>
+        <div class="plus_part">
+            <div class="plus">
+                <p class="number">01</p>
+                <div class="line"></div>
+                <h3 class="plus_name">good price</h3>
+                <p class="plus_description">
+                    I can do any work for you for good price.<br>
+                    I will not try to get more mony for you.<br>
+                    For me the main thing is the quality of work for any money.
+                </p>
+            </div>
+            <div class="img">
+                <img src="res/forest.jpg" alt="">
+            </div>
+        </div>
+        <div class="plus_part">
+            <div class="img">
+                <img src="res/forest_snow.jpg" alt="">
+            </div>
+            <div class="plus">
+                <p class="number">02</p>
+                <div class="line"></div>
+                <h3 class="plus_name">good quality</h3>
+                <p class="plus_description">
+                    I believe that I do my job well.<br>
+                    I am ready to sit all day at the code editor,<br>
+                    but the main thing is to complete the task.
+                </p>
+            </div>
+        </div>
+        <div class="plus_part">
+            <div class="plus">
+                <p class="number">03</p>
+                <div class="line"></div>
+                <h3 class="plus_name">do with love</h3>
+                <p class="plus_description">
+                    I put a lot of effort into any job, <br>
+                    even if it pays little
+                </p>
+            </div>
+            <div class="img">
+                <img src="res/green_forest.jpg" alt="">
+            </div>
+        </div>
+        <div class="line"></div>
+        <p class="up_work_link">for more information go on <a href="https://www.upwork.com/freelancers/~01517b1af176e5a167" title="my account">up work account</a></p>
+    </div>
+    <div class="ready_projects">
+        <h2>my ready projects</h2>
+        <div class="projects_cards">
+            <div class="project_card">
+                <div class="img_background">
+                    <img src="res/cargox_web.png" alt="">
+                </div>
+                <div class="project_information">
+                    <div class="information">
+                        <p class="project_name">cargox web</p>
+                        <div class="line"></div>
+                        <p class="made_time">2022</p>
+                    </div>
+                </div>
+            </div>
+            <div class="project_card">
+                <div class="img_background">
+                    <img src="res/cargox_web.png" alt="">
+                </div>
+                <div class="project_information">
+                    <div class="information">
+                        <p class="project_name">cargox web</p>
+                        <div class="line"></div>
+                        <p class="made_time">2022</p>
+                    </div>
+                </div>
+            </div>
+            <div class="project_card">
+                <div class="img_background">
+                    <img src="res/cargox_web.png" alt="">
+                </div>
+                <div class="project_information">
+                    <div class="information">
+                        <p class="project_name">cargox web</p>
+                        <div class="line"></div>
+                        <p class="made_time">2022</p>
+                    </div>
+                </div>
+            </div>
+            <div class="project_card">
+                <div class="img_background">
+                    <img src="res/cargox_web.png" alt="">
+                </div>
+                <div class="project_information">
+                    <div class="information">
+                        <p class="project_name">cargox web</p>
+                        <div class="line"></div>
+                        <p class="made_time">2022</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php
-    include "layout/footer.php";
+include "layout/footer.php";
 ?>
--- layout/header_line.php
+++ layout/header_line.php
@@ -1,15 +1,26 @@
 <div class="line_container">
     <div class="links_part">
         <div class="web_logo">
-            <img src="res/web_logo.png" alt="">
+            <img src="res/web_logo.png">
         </div>
-        <div><a href="#about_me">About me</a></div>
+        <div><a href="#about_me" class="thirst_link">About me</a></div>
         <div><a href="">Ready Projects</a></div>
         <div><a href="">active projects</a></div>
     </div>
     <div class="links">
+        <div><a href="" title="thank you">apply ↗</a></div>
         <div class="img">
             <img src="res/burger-menu-icon.png" alt="">
+        </div>
+    </div>
+    <div class="line_sub">
+        <div class="sub_part">
+            <h3>Ready Projects</h3>
+            <div class="sub_content">
+                <div>
+                    <a href="">cargox</a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
