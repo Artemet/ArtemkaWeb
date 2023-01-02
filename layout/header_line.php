c3658329444c7fda0edaefe2ef53e27473a08b9b
Artemet <60576459+Artemet@users.noreply.github.com>
2023-01-02 17:35:57 +0200

    Add files via upload

--- layout/header_line.php
+++ layout/header_line.php
@@ -9,7 +9,7 @@
     </div>
     <div class="links">
         <div><a href="" title="thank you">apply ↗</a></div>
-        <div class="img">
+        <div class="burger_menu">
             <img src="res/burger-menu-icon.png" alt="">
         </div>
     </div>
@@ -19,6 +19,52 @@
             <div class="sub_content">
                 <div>
                     <a href="">cargox</a>
+                    <div class="img">
+                        <img src="res/php.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="sub_content">
+                <div>
+                    <a href="">Bright99</a>
+                    <div class="img">
+                        <img src="res/php.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="sub_content">
+                <div>
+                    <a href="">Francesco Donni</a>
+                    <div class="img">
+                        <img src="res/php.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="sub_part">
+            <h3>in developing</h3>
+            <div class="sub_content">
+                <div>
+                    <a href="">Wallpaper Life</a>
+                    <div class="img">
+                        <img src="res/laravel.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="sub_content">
+                <div>
+                    <a href="">The Haleva Bot</a>
+                    <div class="img">
+                        <img src="res/python.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="sub_content">
+                <div>
+                    <a href="">Farmer life</a>
+                    <div class="img">
+                        <img src="res/unreal_engine.png" alt="">
+                    </div>
                 </div>
             </div>
         </div>
